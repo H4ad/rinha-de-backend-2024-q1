f5de28f23bdd5c539a6316fa733d359e23f3316d
Vinícius Lourenço <contact@viniciusl.com.br>
2024-02-14 11:26:25 -0300

    feat(postgres): added support for pg

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,12 @@
-import {Redis} from 'ioredis';
-import {createServer, IncomingMessage, ServerResponse} from 'node:http';
-import {type IValidation, json, tags} from 'typia';
-import {readFileSync} from 'node:fs';
-import {join} from 'node:path';
-import {fileURLToPath} from 'node:url';
+import { createServer, IncomingMessage, ServerResponse } from 'node:http';
+import pg from 'pg';
+import { json, tags, type IValidation } from 'typia';
 
-const redisUrl = process.env.REDIS_URL ?? 'redis://localhost:6379/0';
-const redis = new Redis(redisUrl);
+const {Client} = pg;
 
 const defaultJsonHeaders = {
   'Content-Type': 'application/json',
 };
-
-const currentDir = fileURLToPath(import.meta.url) + './..';
-
-const storeTransactionLua = readFileSync(join(currentDir, './commands/store-transaction.lua'), 'utf8');
-const getTransactionsLua = readFileSync(join(currentDir, './commands/get-transactions.lua'), 'utf8');
-const seedLua = readFileSync(join(currentDir, './commands/seed-data.lua'), 'utf8');
-
-redis.defineCommand('storetransaction', {
-  lua: storeTransactionLua,
-  numberOfKeys: 1,
-});
-
-redis.defineCommand('gettransactions', {
-  lua: getTransactionsLua,
-  numberOfKeys: 1,
-});
-
-redis.defineCommand('seeddata', {
-  lua: seedLua,
-  numberOfKeys: 0,
-});
 
 type Transaction = {
   valor: number & tags.Minimum<0> & tags.Type<'uint32'>;
@@ -58,6 +33,20 @@ const transactionParse = json.createValidateParse<Transaction>();
 const transactionStringify = json.createStringify<TransactionResult>();
 const excerptStringify = json.createStringify<Excerpt>();
 const stringifyTypiaErrors = json.createStringify<{ errors: IValidation.IError[] }>();
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL ?? 'postgres://postgres:hbZkzny5xrvVH@localhost:5432/dev',
+  statement_timeout: 0,
+  query_timeout: 0,
+  connectionTimeoutMillis: 0,
+  keepAlive: true,
+});
+
+client.connect();
+
+client.on('error', (error) => {
+  console.error(error);
+});
 
 const waitPayload = (req: IncomingMessage, callback: (data: string | null) => void) => {
   let body: null | string = null;
@@ -99,7 +88,7 @@ const transactionHandler = (id: number, req: IncomingMessage, res: ServerRespons
     return notFoundHandler(res);
   }
 
-  waitPayload(req, body => {
+  waitPayload(req, async body => {
     if (!body) {
       return unprocessableEntityHandler(res);
     }
@@ -112,29 +101,30 @@ const transactionHandler = (id: number, req: IncomingMessage, res: ServerRespons
       return;
     }
 
-    const description = encodeURIComponent(parsed.data.descricao);
-
-    // @ts-ignore
-    redis['storetransaction'](id, parsed.data.tipo, parsed.data.valor, description, Date.now(), (error, data) => {
+    const args = [id, parsed.data.tipo, parsed.data.valor, parsed.data.descricao];
+    client.query(`CALL SALVAR_TRANSACAO($1, $2, $3, $4, '0'::varchar);`, args, (error: any, result) => {
       if (error) {
+        if (error.constraint === 'pessoas_check') {
+          return unprocessableEntityHandler(res);
+        }
+
+        console.error('Error calling procedure', error);
         return unexpectedError(res);
       }
 
-      if (data === -1) {
+      if (result.rowCount === 0) {
+        console.error('Error zero rows updated');
+        return unexpectedError(res);
+      }
+
+      if (result.rows[0].resultado === '-1') {
         return notFoundHandler(res);
       }
 
-      if (data === -2) {
-        return unprocessableEntityHandler(res);
-      }
-
-      const result = transactionStringify({
-        limite: data[0],
-        saldo: data[1],
-      });
+      const [balance, limit] = result.rows[0].resultado.split(':');
 
       res.writeHead(200, defaultJsonHeaders);
-      res.end(result);
+      res.end(transactionStringify({saldo: +balance, limite: +limit}));
     });
   });
 };
@@ -144,42 +134,34 @@ const extractHandler = (id: number, req: IncomingMessage, res: ServerResponse) =
     return notFoundHandler(res);
   }
 
-  // @ts-ignore
-  redis['gettransactions'](id, (error, data) => {
+  client.query(`SELECT id, limite, saldo FROM pessoas WHERE id = $1;`, [id], (error, result) => {
     if (error) {
+      console.error('Error selecting data', error);
       return unexpectedError(res);
     }
 
-    if (data === -1) {
+    if (result.rowCount === 0) {
       return notFoundHandler(res);
     }
 
-    if (data === -2) {
-      return unprocessableEntityHandler(res);
-    }
+    const data = result.rows[0];
 
-    const transactions: Excerpt['ultimas_transacoes'] = (data[2] || []).map((transactionLog: string) => {
-      const [type, value, description, timestamp] = transactionLog.split(':');
+    client.query(`SELECT valor, tipo, descricao, realizada_em FROM transacoes WHERE pessoa_id = $1 ORDER BY realizada_em DESC LIMIT 10;`, [id], (err2, transactions) => {
+      if (err2) {
+        console.error('Error selecting data of transactions', err2);
+        return unexpectedError(res);
+      }
 
-      return {
-        tipo: type,
-        valor: +value,
-        descricao: description,
-        realizada_em: new Date(+timestamp)
-      };
+      res.writeHead(200, defaultJsonHeaders);
+      res.end(excerptStringify({
+        saldo: {
+          limite: data.limite,
+          total: data.saldo,
+          data_extrato: new Date(),
+        },
+        ultimas_transacoes: transactions.rows,
+      }));
     });
-
-    const result = excerptStringify({
-      saldo: {
-        limite: data[0],
-        total: data[1],
-        data_extrato: new Date(),
-      },
-      ultimas_transacoes: transactions,
-    });
-
-    res.writeHead(200, defaultJsonHeaders);
-    res.end(result);
   });
 }
 
@@ -189,15 +171,8 @@ const server = createServer((req, res) => {
   }
 
   if (req.url === '/health') {
-    // @ts-ignore
-    redis['seeddata']((error) => {
-      if (error) {
-        console.error('Error seeding data', error);
-      }
-
-      res.writeHead(error ? 500 : 204, defaultJsonHeaders);
-      res.end();
-    });
+    res.writeHead(204, defaultJsonHeaders);
+    res.end();
     return;
   }
 
@@ -227,9 +202,9 @@ const server = createServer((req, res) => {
   }
 });
 
-server.keepAliveTimeout = (60 * 1000) + 1000;
-server.headersTimeout = 12 * 1000;
+server.keepAliveTimeout = 60 * 1000;
+server.headersTimeout = 60 * 1000;
 server.maxRequestsPerSocket = 0;
-server.listen(3000);
+server.listen(3000, 5_000);
 
 console.log('Server running at http://localhost:3000/');
